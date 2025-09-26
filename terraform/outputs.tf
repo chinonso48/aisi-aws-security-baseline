3215458f04ebc77f3ea7502d7f9aebf0660af121
chinonso48 <166803338+chinonso48@users.noreply.github.com>
2025-09-26 11:05:24 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,27 +3,30 @@ output "cloudtrail_arn" {
   value       = aws_cloudtrail.main.arn
 }
 
-output "logging_kms_key_arn" {
-  description = "ARN of the logging KMS key"
-  value       = aws_kms_key.logging.arn
-}
+# The following outputs are commented out until the resources are defined
+# -------------------------------------------------------
 
-output "ebs_kms_key_arn" {
-  description = "ARN of the EBS KMS key" 
-  value       = aws_kms_key.ebs.arn
-}
+# output "logging_kms_key_arn" {
+#   description = "ARN of the logging KMS key"
+#   value       = aws_kms_key.logging.arn
+# }
 
-output "data_kms_key_arn" {
-  description = "ARN of the general data KMS key"
-  value       = aws_kms_key.data.arn
-}
+# output "ebs_kms_key_arn" {
+#   description = "ARN of the EBS KMS key" 
+#   value       = aws_kms_key.ebs.arn
+# }
 
-output "guardduty_detector_id" {
-  description = "GuardDuty detector ID"
-  value       = aws_guardduty_detector.main.id
-}
+# output "data_kms_key_arn" {
+#   description = "ARN of the general data KMS key"
+#   value       = aws_kms_key.data.arn
+# }
 
-output "config_recorder_name" {
-  description = "Name of the Config configuration recorder"
-  value       = aws_config_configuration_recorder.main.name
-}
+# output "guardduty_detector_id" {
+#   description = "GuardDuty detector ID"
+#   value       = aws_guardduty_detector.main.id
+# }
+
+# output "config_recorder_name" {
+#   description = "Name of the Config configuration recorder"
+#   value       = aws_config_configuration_recorder.main.name
+# }
